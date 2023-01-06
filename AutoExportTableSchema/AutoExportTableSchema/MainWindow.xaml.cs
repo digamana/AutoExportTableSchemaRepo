d47f235875641fd7911b7c67f88f2338d78a9757
q12389 <q12389@yahoo.com.tw>
2023-01-06 22:54:03 +0800

    Dll加入套件

--- AutoExportTableSchema/AutoExportTableSchema/MainWindow.xaml.cs
+++ AutoExportTableSchema/AutoExportTableSchema/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoExportTableSchema.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace AutoExportTableSchema
         public MainWindow()
         {
             InitializeComponent();
+            Center center = new Center();
         }
 
         private void DownLoadTemplete(object sender, RoutedEventArgs e)
