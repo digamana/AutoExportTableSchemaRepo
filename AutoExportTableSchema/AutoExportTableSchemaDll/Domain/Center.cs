d5cde45649b38f46c018d462a0dc611a776d3aff
q12389 <q12389@yahoo.com.tw>
2023-01-11 12:02:39 +0800

    修正欄位沒有缺少的BUG

--- AutoExportTableSchema/AutoExportTableSchemaDll/Domain/Center.cs
+++ AutoExportTableSchema/AutoExportTableSchemaDll/Domain/Center.cs
@@ -17,7 +17,7 @@ namespace AutoExportTableSchema.Domain
         public static readonly string Downloads = new Syroot.Windows.IO.KnownFolder(Syroot.Windows.IO.KnownFolderType.Downloads).Path;
         ExcelPackage excel { get; set; }
         ExcelWorksheet workSheet { get; set; }
-        public void Run(string ConnectString,string FileName)
+        public void Run(string ConnectString, string FileName)
         {
             excel = new ExcelPackage();
             setExcelTitle();
@@ -29,11 +29,11 @@ namespace AutoExportTableSchema.Domain
             int i = 0;
             int j = 0;
             var tempList = sqlData.lstAttributes().Select(c => c.TableName).Distinct().ToArray();
-            
+
         }
         public void setExcelTitle()
         {
-            
+
             workSheet = excel.Workbook.Worksheets.Add("總表");
             changeBackgroundColor(workSheet, $"A1:I1", Color.Yellow);
             workSheet.Cells[1, 1].Value = "SchemaName";
@@ -79,7 +79,7 @@ namespace AutoExportTableSchema.Domain
             workSheet.Cells[1, 2].Value = "資料庫名稱";
             workSheet.Cells[1, 3].Value = "帳號";
             workSheet.Cells[1, 4].Value = "密碼";
- 
+
             workSheet.Cells[$"A1:D1"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
             workSheet.Cells[$"A1:D1"].Style.Fill.BackgroundColor.SetColor(Color.Yellow);
             workSheet.Cells["A1:D1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
@@ -103,13 +103,13 @@ namespace AutoExportTableSchema.Domain
             Excel excel = new Excel(FilePath, "Sheet1");
             for (int i = 0; i < excel.sheet.Dimension.End.Row; i++)
             {
-                var E1 = excel.sheet.Cells[i+2, 1].Value;
-                var E2 = excel.sheet.Cells[i+2, 2].Value;
-                var E3 = excel.sheet.Cells[i+2, 3].Value;
-                var E4 = excel.sheet.Cells[i+2, 4].Value;
-                if (E1 != null && E2 != null && E3 != null && E4 != null )
+                var E1 = excel.sheet.Cells[i + 2, 1].Value;
+                var E2 = excel.sheet.Cells[i + 2, 2].Value;
+                var E3 = excel.sheet.Cells[i + 2, 3].Value;
+                var E4 = excel.sheet.Cells[i + 2, 4].Value;
+                if (E1 != null && E2 != null && E3 != null && E4 != null)
                 {
-                    lst.Add(new Tempelte(E1,E2,E3,E4));
+                    lst.Add(new Tempelte(E1, E2, E3, E4));
                 }
             }
             return lst;
@@ -120,50 +120,46 @@ namespace AutoExportTableSchema.Domain
             int j = 0;
             var tempList = sqlData.lstAttributes().Select(c => c.TableName).Distinct().ToList();
             //workSheet.Cells[$"B{min}:Z{max}"].Merge = true;
-            int iRange=0;
-            foreach (var item in sqlData.lstAttributes())
+            int iRange = 0;
+            var temp2222 = sqlData.lstAttributes().Where(c => c.TableName == "ZRFC_MM08").ToList();
+            //Excel總行數 =  sqlData.lstAttributes().Count() + Excel標題  +  sqlData.lstAttributes().Select(c => c.TableName).Distinct().Count();
+            foreach (var item in tempList)
             {
-                if (j<= tempList.Count()-1 && item.TableName != tempList[j])
+                workSheet.Cells[$"A{2 + i}:I{2 + i}"].Merge = true;
+                workSheet.Cells[2 + i, 1].Value = item;
+                workSheet.Cells[2 + i, 2].Value = "";
+                workSheet.Cells[2 + i, 3].Value = "";
+                workSheet.Cells[2 + i, 4].Value = "";
+                workSheet.Cells[2 + i, 5].Value = "";
+                workSheet.Cells[2 + i, 6].Value = "";
+                workSheet.Cells[2 + i, 7].Value = "";
+
+                workSheet.Cells[$"A{2 + i}"].Value = workSheet.Cells[$"B{2 + i}"].Value;
+                workSheet.Cells[$"A{2 + i}"].Style.Font.Size = 20;
+                workSheet.Cells[$"A{2 + i}"].Style.Font.Bold = true;
+                workSheet.Cells[$"A{2 + i}:I{2 + i}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                workSheet.Cells[$"A{2 + i}:I{2 + i}"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                changeBackgroundColor(workSheet, $"A{2 + i}:I{2 + i}", Color.FromArgb(255, 127, 80));
+                workSheet.Row(3 + i).Height = 25;
+                var tableName = sqlData.lstAttributes().Where(c => c.TableName == item).ToList();
+                foreach (var item2 in tableName)
                 {
-                    j++;
-                    workSheet.Cells[3 + i, 1].Value = "";
-                    workSheet.Cells[3 + i, 2].Value = "";
-                    workSheet.Cells[3 + i, 3].Value = "";
-                    workSheet.Cells[3 + i, 4].Value = "";
-                    workSheet.Cells[3 + i, 5].Value = "";
-                    workSheet.Cells[3 + i, 6].Value = "";
-                    workSheet.Cells[3 + i, 7].Value = "";
-                    workSheet.Cells[$"A{3+i}:I{3+i}"].Merge = true;
-                    workSheet.Cells[$"A{3 + i}"].Value = workSheet.Cells[$"B{3 + i}"].Value;
-                    workSheet.Cells[$"A{3 + i}"].Style.Font.Size = 20;
-                    workSheet.Cells[$"A{3 + i}"].Style.Font.Bold = true;
-                    workSheet.Cells[$"A{3 + i}:I{3 + i}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-                    workSheet.Cells[$"A{3 + i}:I{3 + i}"].Style.VerticalAlignment = ExcelVerticalAlignment.Center;
-                    changeBackgroundColor(workSheet, $"A{3 + i}:I{3 + i}", Color.FromArgb(255, 127, 80));
-                    workSheet.Row(3 + i).Height = 25;
-
+                    i++;
+                    workSheet.Cells[2 + i, 1].Value = item2.SchemaName;
+                    workSheet.Cells[2 + i, 2].Value = item2.TableName;
+                    workSheet.Cells[2 + i, 3].Value = item2.ColumnName;
+                    workSheet.Cells[2 + i, 4].Value = item2.ColumnType;
+                    workSheet.Cells[2 + i, 5].Value = item2.MaxLength;
+                    workSheet.Cells[2 + i, 6].Value = item2.IsNull;
+                    workSheet.Cells[2 + i, 7].Value = item2.IsPrimaryKey;
+                    changeBackgroundColor(workSheet, $"A{2 + i}:I{2 + i}", Color.FromArgb(255, 228, 225));
                 }
-                else
-                {
-                    workSheet.Cells[3 + i, 1].Value = item.SchemaName;
-                    workSheet.Cells[3 + i, 2].Value = item.TableName;
-                    workSheet.Cells[3 + i, 3].Value = item.ColumnName;
-                    workSheet.Cells[3 + i, 4].Value = item.ColumnType;
-                    workSheet.Cells[3 + i, 5].Value = item.MaxLength;
-                    workSheet.Cells[3 + i, 6].Value = item.IsNull;
-                    workSheet.Cells[3 + i, 7].Value = item.IsPrimaryKey;
-                    changeBackgroundColor(workSheet, $"A{3 + i}:I{3 + i}", Color.FromArgb(255, 228, 225));
-                    iRange++;
-                }
-
-
-                
                 i++;
             }
 
-         
 
-            var modelTable = workSheet.Cells[$"A1:I{sqlData.lstAttributes().Count()+2}"];
+
+            var modelTable = workSheet.Cells[$"A1:I{sqlData.lstAttributes().Count() + tempList.Count() + 1}"];
             workSheet.Cells[$"A1:I{sqlData.lstAttributes().Count()}"].AutoFitColumns();
             workSheet.Column(8).Width = 30;
             // Assign borders
@@ -173,7 +169,7 @@ namespace AutoExportTableSchema.Domain
             modelTable.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
             IExcelSkill skill = new ExcelSkill(workSheet);
             skill.setMergeValue(tempList);
-            var tempeee=skill.getMergeRange(out int last);
+            var tempeee = skill.getMergeRange(out int last);
             foreach (var item in tempeee)
             {
                 workSheet.Cells[$"I{item.Min}:I{item.Max}"].Merge = true;
@@ -181,18 +177,8 @@ namespace AutoExportTableSchema.Domain
                 workSheet.Cells[$"A{item.Min}:I{item.Max}"].Style.VerticalAlignment = ExcelVerticalAlignment.Top;
                 workSheet.Cells[$"I{item.Min}:I{item.Max}"].Style.Font.Name = "微軟正黑體";
             }
-            workSheet.Cells[$"I{last+1}:I{i + 2}"].Merge = true;
+            workSheet.Cells[$"I{last + 1}:I{i + 1}"].Merge = true;
             workSheet.Column(9).Width = 30;
-            //var lst = skill.getMergeRoews().ToList();
-            //var resLst = new List<Limit>();
-            //for (int ii=0;ii < lst.Count() -2;ii++)
-            //{
-            //    var a1 = lst[ii] + 1;
-            //    var a2= lst[ii+1] -1;
-
-            //    resLst.Add(new Limit(lst[ii] + 1, lst[ii] - 1));
-            //}
-
         }
         public void changeBackgroundColor(ExcelWorksheet workSheet, string Range, Color color)
         {
